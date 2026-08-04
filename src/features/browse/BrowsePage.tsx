e8b897ff3944ee2c7b26ca37258141534ef1272a
MuqhtadeerM <muhammedmuzawar9@gmail.com>
2026-08-04 15:21:51 +0530

    fix: usues with layout

--- src/features/browse/BrowsePage.tsx
+++ src/features/browse/BrowsePage.tsx
@@ -27,33 +27,3 @@ export default function BrowsePage() {
     </Container>
   );
 }
-
-// import Container from "@/shared/components/Container";
-// import HeroBanner from "./components/HeroBanner";
-// import GenreSection from "./components/GenreSection";
-// import NewArrivalsSection from "./components/NewArrivalsSection";
-
-// export default function BrowsePage() {
-//   return (
-//     <>
-//       {/* Top Section */}
-//       <section
-//         className="
-//           w-full
-//           bg-[#FFFDF3]
-//         "
-//       >
-//         <Container className="flex flex-col gap-14 pt-[10px] pb-[50px]">
-//           <HeroBanner />
-
-//           <GenreSection />
-//         </Container>
-//       </section>
-
-//       {/* Remaining Page */}
-//       <Container className="pt-[56px] pb-24">
-//         <NewArrivalsSection />
-//       </Container>
-//     </>
-//   );
-// }
